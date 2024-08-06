b4ad4186b02132c3ef0ffaa2a6bbd0ae7acb8a5c
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2024-08-06 16:59:36 +0300

    Update UsersController.js

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,6 +1,7 @@
 // controllers/UsersController.js
-const sha1 = require('sha1');
 const dbClient = require('../utils/db');
+const redisClient = require('../utils/redis');
+const sha1 = require('sha1');
 
 class UsersController {
   static async postNew(req, res) {
@@ -25,6 +26,25 @@ class UsersController {
     const result = await usersCollection.insertOne({ email, password: hashedPassword });
 
     return res.status(201).json({ id: result.insertedId, email });
+  }
+
+  static async getMe(req, res) {
+    const token = req.header('X-Token');
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const userId = await redisClient.get(`auth_${token}`);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const user = await dbClient.db.collection('users').findOne({ _id: new ObjectId(userId) });
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    return res.status(200).json({ id: user._id, email: user.email });
   }
 }
 

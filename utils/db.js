d0a4b0577757a387434a26e5046add715b275ad6
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2024-08-06 15:03:26 +0300

    Update db.js

---
+++ utils/db.js
@@ -0,0 +1,38 @@
+// utils/db.js
+const { MongoClient } = require('mongodb');
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}`;
+    this.client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+    this.client.connect()
+      .then(() => {
+        this.db = this.client.db(database);
+      })
+      .catch((err) => {
+        console.error('MongoDB connection error:', err);
+      });
+  }
+
+  isAlive() {
+    return this.client && this.client.isConnected();
+  }
+
+  async nbUsers() {
+    const usersCollection = this.db.collection('users');
+    const count = await usersCollection.countDocuments();
+    return count;
+  }
+
+  async nbFiles() {
+    const filesCollection = this.db.collection('files');
+    const count = await filesCollection.countDocuments();
+    return count;
+  }
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
